a654b66ba02869f9c676558f50280f025e9a29b7
Angelica Beltran <angelica.beltran11@gmail.com>
2023-05-10 12:28:00 -0500

    talked to liz

--- server/public/client.js
+++ server/public/client.js
@@ -1,21 +1,54 @@
 $(document).ready(onReady);
 
 function onReady() {
-    $.ajax({
-        type: 'GET',
-        url: '/artist'
-    }).then(function (response) {
-        for (let i = 0; i < response.length; i++) {
-            let artist = response[i];
-            $('#artistTableBody').append(`
-                <tr>
-                    <td>${artist.name}</td>
-                    <td>${artist.born}</td>
-                    <td>${artist.died}</td>
-                </tr>
-            `);
-        }
-    });
+//   $.ajax({
+//     type: "GET",
+//     url: "/artist",
+//   }).then(function (response) {
+//     for (let i = 0; i < response.length; i++) {
+//       let artist = response[i];
+//     }
+  
 
-    // TODO Add ajax request for /songs and display on DOM
+//   // TODO Add ajax request for /songs and display on DOM
+// } //end of on Ready
+//   )
+getArtists();
+getSongs();
+}
+
+function getArtists() {
+  $.ajax({
+    type: "GET",
+    url: "/artist",
+  }).then(function (response) {
+    for (let i = 0; i < response.length; i++) {
+      let artist = response[i];
+      $("#artistTableBody").append(`
+      <tr>
+          <td>${artist.name}</td>
+          <td>${artist.born}</td>
+          <td>${artist.died}</td>
+      </tr>
+  `);
+}
+});
+              
+} //end of getArtists
+
+function getSongs() {
+  $.ajax({
+    type: "GET",
+    url: "/song",
+  }).then(function (response) {
+    for (let i = 0; i < response.length; i++) {
+      let music = response[i];
+      $("#songTableBody").append(`
+            <tr>
+                <td>${music.title}</td>
+                <td>${music.artist}</td>
+            </tr>
+                `);
+    }
+  });
 }
